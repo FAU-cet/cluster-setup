24c91ce67d7dcc3d31a6adedd866ae84c724c551
jojodicus <jojodicus@gmail.com>
2024-08-29 16:36:10 +0200

    poweron scripts

--- poweron.sh
+++ poweron.sh
@@ -10,10 +10,11 @@ fi
 wakeup () {
   wol $1
   echo "Sent signal to $2 ($1)"
-  sleep 10 # prevent tripping braker
+  sleep ${3:-10} # prevent tripping braker
 }
 
 wakeup 90:1b:0e:3d:d3:3d viz
+echo "Press Ctrl+C within the next 10 seconds to stop waking nodes"
 wakeup 10:ff:e0:06:2e:dc drain
 wakeup 10:ff:e0:06:2e:d0 hot
-wakeup 10:ff:e0:06:2f:22 cold
+wakeup 10:ff:e0:06:2f:22 cold 0
